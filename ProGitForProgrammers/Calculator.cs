5ad8f5544a4fbcf25f2f9f6f6e6c1c6df9491bae
iGaryG <gaurav.gavas700@gmail.com>
2021-06-19 17:34:09 +0530

    Add the add method

--- ProGitForProgrammers/Calculator.cs
+++ ProGitForProgrammers/Calculator.cs
@@ -2,5 +2,10 @@ namespace ProGitForProgrammers
 {
     public class Calculator
     {
+        public int Add (int left, int right)
+        {
+            return left + right;
+        }
+
     }
 }
